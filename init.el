e4a0a8b63dc29d2d2679b4be9e7d10e5a8344b92
George Kettleborough <g.kettleborough@member.fsf.org>
2012-03-01 08:45:07 +0000

    Gnus sort threads by date

--- init.el
+++ init.el
@@ -733,7 +733,7 @@ call to other-window-repeat or switch-prev-window."
  gnus-summary-line-format "%U%R%z %(%&user-date;  %-15,15f  %B%s%)\n"
  gnus-user-date-format-alist '((t . "%Y-%m-%d %H:%M"))
  gnus-summary-thread-gathering-function 'gnus-gather-threads-by-references
- gnus-thread-sort-functions '((not gnus-thread-sort-by-most-recent-number))
+ gnus-thread-sort-functions '((not gnus-thread-sort-by-most-recent-date))
  gnus-sum-thread-tree-false-root ""
  gnus-sum-thread-tree-indent " "
  gnus-sum-thread-tree-leaf-with-other "├► "
