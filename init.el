2a6ad36ceb6919a975e32a720cca4b68df68fbc1
George Kettleborough (CMP) <g.kettleborough@uea.ac.uk>
2012-07-05 10:48:46 +0100

    Paredit in SLIME REPL

--- init.el
+++ init.el
@@ -155,6 +155,15 @@
             (nice-paredit-on)
             (local-set-key (kbd "C-c z") 'slime-switch-to-output-buffer)))
 
+;;; REPL
+(add-hook 'slime-repl-mode-hook (lambda () (paredit-mode +1)))
+;; Stop SLIME's REPL from grabbing DEL,
+;; which is annoying when backspacing over a '('
+(defun override-slime-repl-bindings-with-paredit ()
+  (define-key slime-repl-mode-map
+    (read-kbd-macro paredit-backward-delete-key) nil))
+(add-hook 'slime-repl-mode-hook 'override-slime-repl-bindings-with-paredit)
+
 ;;; *** org-mode settings ***
 (require 'org-install)
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
