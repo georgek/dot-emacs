b5f6c5eed8299ac0ef646343144704e3a108c5b6
George Kettleborough <g.kettleborough@member.fsf.org>
2013-04-13 23:11:54 +0100

    Disable for new org version

--- init.el
+++ init.el
@@ -266,36 +266,36 @@ RECURRENCES occasions."
   "Convert a number of minutes to an HHMM time"
   (+ (* (/ minutes 60) 100) (% minutes 60)))
 
-(defadvice org-agenda-add-time-grid-maybe (around mde-org-agenda-grid-tweakify
-                                                  (list ndays todayp))
-  (if (member 'remove-match (car org-agenda-time-grid))
-      (cl-flet ((extract-window
-              (line)
-              (let ((start (get-text-property 1 'time-of-day line))
-                    (dur (get-text-property 1 'duration line)))
-                (cond
-                 ((and start dur)
-                  (cons start
-                        (org-time-from-minutes
-                         (+ dur (org-time-to-minutes start)))))
-                 (start start)
-                 (t nil)))))
-        (let* ((windows (delq nil (mapcar 'extract-window list)))
-               (org-agenda-time-grid
-                (list (car org-agenda-time-grid)
-                      (cadr org-agenda-time-grid)
-                      (remove-if
-                       (lambda (time)
-                         (find-if (lambda (w)
-                                    (if (numberp w)
-                                        (equal w time)
-                                      (and (>= time (car w))
-                                           (< time (cdr w)))))
-                                  windows))
-                       (caddr org-agenda-time-grid)))))
-          ad-do-it))
-    ad-do-it))
-(ad-activate 'org-agenda-add-time-grid-maybe)
+;; (defadvice org-agenda-add-time-grid-maybe (around mde-org-agenda-grid-tweakify
+;;                                                   (list ndays todayp))
+;;   (if (member 'remove-match (car org-agenda-time-grid))
+;;       (cl-flet ((extract-window
+;;               (line)
+;;               (let ((start (get-text-property 1 'time-of-day line))
+;;                     (dur (get-text-property 1 'duration line)))
+;;                 (cond
+;;                  ((and start dur)
+;;                   (cons start
+;;                         (org-time-from-minutes
+;;                          (+ dur (org-time-to-minutes start)))))
+;;                  (start start)
+;;                  (t nil)))))
+;;         (let* ((windows (delq nil (mapcar 'extract-window list)))
+;;                (org-agenda-time-grid
+;;                 (list (car org-agenda-time-grid)
+;;                       (cadr org-agenda-time-grid)
+;;                       (remove-if
+;;                        (lambda (time)
+;;                          (find-if (lambda (w)
+;;                                     (if (numberp w)
+;;                                         (equal w time)
+;;                                       (and (>= time (car w))
+;;                                            (< time (cdr w)))))
+;;                                   windows))
+;;                        (caddr org-agenda-time-grid)))))
+;;           ad-do-it))
+;;     ad-do-it))
+;; (ad-activate 'org-agenda-add-time-grid-maybe)
 
 ;; *** end org-mode stuff ***
 
