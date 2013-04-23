6aefc7bd51e45ab6b6cd72978ae4add331d900cd
George Kettleborough <g.kettleborough@member.fsf.org>
2013-04-23 21:48:49 +0100

    Fix modeline a bit

--- gk-diminish.el
+++ gk-diminish.el
@@ -1,22 +1,32 @@
 ;;; here are my settings for a diminished modeline
 (defmacro gk-dim-maj (mode diminished-name)
-  (let ((hook-name (concat (symbol-name mode) "-hook"))
-        (dim-name (concat diminished-name " ")))
+  (let ((hook-name (concat (symbol-name mode) "-hook")))
     `(add-hook ',(intern-soft hook-name)
                (lambda ()
-                 (setq mode-name ,dim-name)))))
+                 (setq mode-name ,diminished-name)))))
 
 (defmacro gk-dim-min (name mode diminished-name)
   `(eval-after-load ,name
      '(diminish ',mode ,diminished-name)))
 
+(setf (caadr (nth 4 mode-line-modes)) " ")
+
 (gk-dim-maj emacs-lisp-mode "Elisp")
 (gk-dim-maj org-mode "Org")
 (gk-dim-maj ielm-mode "IELM")
 
-(gk-dim-min "elisp-slime-nav" elisp-slime-nav-mode "S")
+;;; all
 (gk-dim-min "yasnippet" yas-minor-mode "Y")
-(gk-dim-min "eldoc" eldoc-mode "E")
-(gk-dim-min "paredit" paredit-mode "P")
-(gk-dim-min "macrostep" macrostep-mode "M")
 (gk-dim-min "flyspell" flyspell-mode "F")
+(gk-dim-min "abbrev" abbrev-mode "A")
+
+;;; lisp
+(gk-dim-min "paredit" paredit-mode "P")
+(gk-dim-min "eldoc" eldoc-mode "E")
+
+;;; elisp
+(gk-dim-min "elisp-slime-nav" elisp-slime-nav-mode "S")
+(gk-dim-min "macrostep" macrostep-mode "M")
+
+;;; common lisp
+(gk-dim-min "slime" slime-mode "S")
