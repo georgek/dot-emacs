d1732163cd2598b6d10d1ab204d049426580ad64
George Kettleborough <g.kettleborough@member.fsf.org>
2013-06-12 11:19:24 +0100

    Hash table macro

--- gk-utils.el
+++ gk-utils.el
@@ -7,3 +7,14 @@
   "Makes a multiline string."
   (mapconcat #'identity strings "\n"))
 
+(defmacro hash-table (test &rest contents)
+  (let ((tbl-sym (gensym "tbl"))
+        (conts (mapcar
+                #'(lambda (c)
+                    (cons (eval (car c)) (eval (cadr c))))
+                contents)))
+   `(let ((,tbl-sym (make-hash-table :test ,test)))
+      (dolist (pair ',conts)
+        (puthash (car pair) (cdr pair) ,tbl-sym))
+      ,tbl-sym)))
+
