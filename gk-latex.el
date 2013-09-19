1ca5f4ff281100e14d27640d7d6920fd6857c025
George Kettleborough <g.kettleborough@member.fsf.org>
2013-09-19 19:35:48 +0100

    Add legend option to gk-pgfplots-add-table

--- gk-latex.el
+++ gk-latex.el
@@ -1,13 +1,16 @@
 ;;; utilities for LaTeX
 
-(defun gk-pgfplots-add-table (filename)
+(defun gk-pgfplots-add-table (filename &optional legend)
   (interactive
-   (list (read-file-name "File: ")))
+   (list (read-file-name "File: ")
+         (read-string "Legend: ")))
   (let* ((table-string (file-to-string filename))
          (table-header (car (split-string table-string "\n" t)))
          (x (completing-read "x: " (split-string table-header " " t)))
          (y (completing-read "y: " (split-string table-header " " t))))
     (insert (format "\\addplot table[x=%s,y=%s] {\n" x y)
             table-string
-            "};\n")))
+            "};\n")
+    (unless (string= legend "")
+      (insert (format "\\addlegendentry{%s}\n" legend)))))
 
