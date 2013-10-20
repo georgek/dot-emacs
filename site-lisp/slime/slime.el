6ec93cddf9e2fb3cc414b8c7f2db4f0983bc6c6e
George Kettleborough <g.kettleborough@member.fsf.org>
2013-10-20 23:31:52 +0100

    Improve SLIME completion

--- site-lisp/slime/slime.el
+++ site-lisp/slime/slime.el
@@ -3787,9 +3787,9 @@ Perform completion more similar to Emacs' complete-symbol."
                    (when (member partial completions)
                      (slime-minibuffer-respecting-message
                       "Complete but not unique"))
-                   ;; (slime-display-or-scroll-completions completions
-                   ;;                                      partial)
-                   )))))))
+                   (when (eq this-command last-command)
+                     (slime-display-or-scroll-completions completions
+                                                          partial)))))))))
 
 (defun slime-maybe-complete-as-filename ()
   "If point is at a string starting with \", complete it as filename.
