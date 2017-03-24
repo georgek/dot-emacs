1b1622c8b17ab4bb2bc9b97b1f2bf94dfaf76f3c
George Kettleborough <george.kettleborough@earlham.ac.uk>
2017-03-24 16:51:44 +0000

    Add better binding for org up and down

--- init.el
+++ init.el
@@ -324,6 +324,14 @@
 ;;; *** org-mode settings ***
 (require 'org)
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+
+(makehookedfun org-mode-hook
+  (auto-fill-mode)
+  (org-bullets-mode 1)
+  (flyspell-mode 1)
+  (local-set-key (kbd "M-p") #'org-metaup)
+  (local-set-key (kbd "M-n") #'org-metadown))
+
 (setq org-directory "~/org/")
 (defmacro orgdr (&optional filename)
   (if filename
@@ -439,45 +447,9 @@ RECURRENCES occasions."
   "Convert a number of minutes to an HHMM time"
   (+ (* (/ minutes 60) 100) (% minutes 60)))
 
-;; (defadvice org-agenda-add-time-grid-maybe (around mde-org-agenda-grid-tweakify
-;;                                                   (list ndays todayp))
-;;   (if (member 'remove-match (car org-agenda-time-grid))
-;;       (cl-flet ((extract-window
-;;               (line)
-;;               (let ((start (get-text-property 1 'time-of-day line))
-;;                     (dur (get-text-property 1 'duration line)))
-;;                 (cond
-;;                  ((and start dur)
-;;                   (cons start
-;;                         (org-time-from-minutes
-;;                          (+ dur (org-time-to-minutes start)))))
-;;                  (start start)
-;;                  (t nil)))))
-;;         (let* ((windows (delq nil (mapcar 'extract-window list)))
-;;                (org-agenda-time-grid
-;;                 (list (car org-agenda-time-grid)
-;;                       (cadr org-agenda-time-grid)
-;;                       (remove-if
-;;                        (lambda (time)
-;;                          (find-if (lambda (w)
-;;                                     (if (numberp w)
-;;                                         (equal w time)
-;;                                       (and (>= time (car w))
-;;                                            (< time (cdr w)))))
-;;                                   windows))
-;;                        (caddr org-agenda-time-grid)))))
-;;           ad-do-it))
-;;     ad-do-it))
-;; (ad-activate 'org-agenda-add-time-grid-maybe)
-
 ;;; setup default file readers
 (eval-after-load "org"
   '(setcdr (assoc "\\.pdf\\'" org-file-apps) "evince %s"))
-
-(makehookedfun org-mode-hook
-  (auto-fill-mode)
-  (org-bullets-mode 1)
-  (flyspell-mode 1))
 
 ;; *** end org-mode stuff ***
 
