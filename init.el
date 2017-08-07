2c7c434d55e8c87c5ca323044987cb6ec052c509
George Kettleborough <george.kettleborough@earlham.ac.uk>
2017-08-07 10:18:43 +0100

    Make reveal work

--- init.el
+++ init.el
@@ -382,7 +382,7 @@
 (require 'htmlize)
 ;;; org-reveal
 (require 'ox-reveal)
-(setq org-reveal-root (concat "file://" init-path "/misc/reveal.js"))
+(setq org-reveal-root ".")
 (setq org-reveal-init-script "zoomKey: 'shift'")
 
 ;;; twbs
