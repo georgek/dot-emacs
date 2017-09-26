23cb6dc2e02231c37a5b2e6baa1a9840dcf3a57f
George Kettleborough <kettleg@gmail.com>
2017-09-26 21:31:46 +0100

    auctex elpa

--- site-lisp/auctex/style/dk.el
+++
@@ -1,9 +0,0 @@
-;;; dk.el - Setup AUCTeX for editing Danish text.
-
-;;; Code:
-
-(TeX-add-style-hook "dk"
- (function (lambda ()
-   (run-hooks 'TeX-language-dk-hook))))
-
-;;; dk.el ends here
--- site-lisp/auctex/style/dutch.el
+++
@@ -1,9 +0,0 @@
-;;; dutch.el - Setup AUCTeX for editing Dutch text.
-
-;;; Code:
-
-(TeX-add-style-hook "dutch"
- (function (lambda ()
-   (run-hooks 'TeX-language-nl-hook))))
-
-;;; dutch.el ends here
