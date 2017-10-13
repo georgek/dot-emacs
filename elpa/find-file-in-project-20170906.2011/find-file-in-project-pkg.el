47d6cb54aae751867426c0dca5d2790126bbda60
George Kettleborough <kettleg@gmail.com>
2017-10-13 21:23:01 +0100

    Update packages

---
+++ elpa/find-file-in-project-20170906.2011/find-file-in-project-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "find-file-in-project" "20170906.2011" "Find file/directory and review Diff/Patch/Commit efficiently everywhere" '((ivy "0.7.0") (emacs "24.3")) :commit "dcc0801c4781d76efd9ac0d86bab4084b4b93663" :url "https://github.com/technomancy/find-file-in-project" :keywords '("project" "convenience"))
---
+++ elpa/yasnippet-snippets-20171010.1224/yasnippet-snippets-pkg.el
@@ -0,0 +1,7 @@
+(define-package "yasnippet-snippets" "20171010.1224" "Collection of yasnippet snippets"
+  '((yasnippet "0.8.0"))
+  :keywords
+  '("snippets"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
