d28b54bccecfb6a7dcbb072c2746c04e6c754886
George Kettleborough <george.kettleborough@aviva.com>
2017-10-30 08:52:56 +0000

    Use http melpa

--- init.el
+++ init.el
@@ -61,10 +61,7 @@
 (load-init "gk-utils")
 
 (require 'package)
-(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
-                    (not (gnutls-available-p))))
-       (url (concat (if no-ssl "http" "https") "://melpa.org/packages/")))
-  (add-to-list 'package-archives (cons "melpa" url) t))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 (package-initialize)
 
