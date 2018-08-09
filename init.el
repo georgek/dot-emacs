f5e116685ed9277362007924693e629eaeeaab53
George Kettleborough <kettleg@gmail.com>
2018-08-09 21:51:02 +0100

    Eshell

--- init.el
+++ init.el
@@ -524,6 +524,9 @@ RECURRENCES occasions."
                        (auto-fill-mode)
                        (setq tab-stop-list (number-sequence 3 45 3))))))
 
+(use-package eshell
+  :bind (("C-c s" . eshell)))
+
 (use-package man
   :defer t
   :config (setq Man-width 80))
