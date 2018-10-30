ce3da9a6f463ca9e59ff9c52dbcf0aed4ccdf3f7
George Kettleborough <george.kettleborough@owlstone.co.uk>
2018-10-30 10:15:47 +0000

    Add rename and move buffer file functions

--- lisp/gk-extra.el
+++ lisp/gk-extra.el
@@ -56,4 +56,39 @@ to C++."
       ;; not daemon
     (save-buffers-kill-emacs)))
 
+;;; stuff from https://sites.google.com/site/steveyegge2/my-dot-emacs-file
+;;;###autoload
+(defun rename-file-and-buffer (new-name)
+  "Renames both current buffer and file it's visiting to NEW-NAME."
+  (interactive "sNew name: ")
+  (let ((name (buffer-name))
+        (filename (buffer-file-name)))
+    (if (not filename)
+        (message "Buffer '%s' is not visiting a file!" name)
+      (if (get-buffer new-name)
+          (message "A buffer named '%s' already exists!" new-name)
+        (progn (rename-file filename new-name 1)
+               (rename-buffer new-name)
+               (set-visited-file-name new-name)
+               (set-buffer-modified-p nil))))))
+
+;;;###autoload
+(defun move-buffer-file (dir)
+  "Moves both current buffer and file it's visiting to DIR."
+  (interactive "DNew directory: ")
+  (let* ((name (buffer-name))
+         (filename (buffer-file-name))
+         (dir
+          (if (string-match dir "\\(?:/\\|\\\\)$")
+              (substring dir 0 -1) dir))
+         (newname (concat dir "/" name)))
+
+    (if (not filename)
+        (message "Buffer '%s' is not visiting a file!" name)
+      (progn (copy-file filename newname 1)
+             (delete-file filename)
+             (set-visited-file-name newname)
+             (set-buffer-modified-p nil)
+             t))))
+
 (provide 'gk-extra)
