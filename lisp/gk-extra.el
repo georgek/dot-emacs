1a371273eea1ff72fcd3bc55a609d923b2e116e8
George Kettleborough <george.kettleborough@owlstone.co.uk>
2019-09-16 12:47:59 +0100

    gk-extra fixes

--- lisp/gk-extra.el
+++ lisp/gk-extra.el
@@ -4,7 +4,7 @@
 
 ;;;###autoload
 (defun gk-select-current-line ()
-  "Sets the region to the current line"
+  "Set the region to the current line."
   (interactive)
   (beginning-of-line)
   (push-mark nil 1 1)
@@ -12,7 +12,7 @@
 
 ;;;###autoload
 (defun gk-comment-current-line ()
-  "Toggles comment on the current line"
+  "Toggle comment on the current line."
   (interactive)
   (save-excursion
     (gk-select-current-line)
@@ -20,9 +20,10 @@
 
 ;;;###autoload
 (defun gk-c-c++-header ()
-  "Decides whether a .h file is C or C++ based on existence of a
-corresponding .c file. If no such file is found the mode is set
-to C++."
+  "Decide whether a .h file is C or C++ header.
+
+The decision is based on existence of a corresponding .c file.
+If no such file is found the mode is set to C++."
   (interactive)
   (let ((c-file (concat (substring (buffer-file-name) 0 -1) "c")))
     (if (file-exists-p c-file)
@@ -31,7 +32,7 @@ to C++."
 
 ;;;###autoload
 (defun gk-c-c++-toggle ()
-  "Toggles between c-mode and c++-mode"
+  "Toggle between `c-mode' and `c++-mode'."
   (interactive)
   (cond ((string= major-mode "c-mode")
          (c++-mode))
@@ -40,14 +41,16 @@ to C++."
 
 ;;;###autoload
 (defun gk-kill-client-or-daemon ()
-  "Kills emacs.  If running as daemon, the daemon is killed when
-  the last frame is killed."
+  "Kill Emacs client or daemon.
+
+If we are running as daemon, the daemon is killed when the last
+frame is killed."
   (interactive)
   (if (and (boundp 'server-clients)
            (> (length server-clients) 0))
       ;; daemon
       (if (<= (length (frame-list)) 2)
-          (when (y-or-n-p "Last frame. Kill daemon?")
+          (when (y-or-n-p "Last frame; kill daemon? ")
             (save-some-buffers)
             (delete-frame)
             (kill-emacs))
@@ -60,7 +63,10 @@ to C++."
 ;;;###autoload
 (defun rename-file-and-buffer (new-name)
   "Renames both current buffer and file it's visiting to NEW-NAME."
-  (interactive "sNew name: ")
+  (interactive (list
+                (read-file-name
+                 "New filename: "
+                 nil (buffer-file-name) nil)))
   (let ((name (buffer-name))
         (filename (buffer-file-name)))
     (if (not filename)
@@ -74,8 +80,11 @@ to C++."
 
 ;;;###autoload
 (defun move-buffer-file (dir)
-  "Moves both current buffer and file it's visiting to DIR."
-  (interactive "DNew directory: ")
+  "Move both current buffer and file it's visiting to DIR."
+  (interactive (list
+                (read-directory-name
+                 "New directory: "
+                 (file-name-directory (buffer-file-name)))))
   (let* ((name (buffer-name))
          (filename (buffer-file-name))
          (dir
@@ -92,3 +101,4 @@ to C++."
              t))))
 
 (provide 'gk-extra)
+;;; gk-extra.el ends here
