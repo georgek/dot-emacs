ca29d5aff075ce6154b3270aab014538a14ac002
George Kettleborough <kettleg@gmail.com>
2023-08-29 20:23:19 +0100

    Add git-commit-major-mode

--- .dir-locals.el
+++ .dir-locals.el
@@ -4,4 +4,6 @@
  ("lib/undo-tree"
   (emacs-lisp-mode
    (outline-regexp . ";;;\\(;* [^=\t\n]\\|###autoload\\)\\|(")))
+ (git-commit-mode
+  (git-commit-major-mode . git-commit-elisp-text-mode))
  )
