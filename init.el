f939cc2dc3c11d837a008aa0af8a0f12fa69e621
George Kettleborough <george.kettleborough@owlstone.co.uk>
2024-02-12 18:25:20 +0000

    Assimilate 2 drones
    Assimilate magit-todos 1.7-6-gdebb77b
    Assimilate pcre2el     1.12-1-g018531b

--- init.el
+++ init.el
@@ -711,6 +711,10 @@ indent whitespace in front of the next line."
   (setq magit-list-refs-sortby "-creatordate")
   (setq magit-refs-pad-commit-counts t))
 
+(use-package magit-todos
+  :after magit
+  :config (magit-todos-mode 1))
+
 (use-package magit-wip
   :after magit
   :config (magit-wip-mode))
