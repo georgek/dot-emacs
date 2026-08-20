bc7e55b9bee5e160018ff21029c0e2dda7be8d7f
George Kettleborough <george.kettleborough@owlstone.co.uk>
2026-08-20 14:40:46 +0100

    Add treesit ABI comment

--- init.el
+++ init.el
@@ -1636,6 +1636,7 @@ RECURRENCES occasions."
      (json "https://github.com/tree-sitter/tree-sitter-json")
      (make "https://github.com/alemuller/tree-sitter-make")
      (markdown "https://github.com/ikatyang/tree-sitter-markdown")
+     ;; New Python versions use an ABI that isn't support by Emacs 30
      (python "https://github.com/tree-sitter/tree-sitter-python" "v0.23.6")
      (rust "https://github.com/tree-sitter/tree-sitter-rust")
      (toml "https://github.com/tree-sitter/tree-sitter-toml")
